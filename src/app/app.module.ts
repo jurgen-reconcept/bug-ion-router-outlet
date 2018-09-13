8db4fb16a1823bbce0b5ac53f2708527227819ab
Jürgen Visser <jurgen@reconcept.nl>
2018-09-13 23:41:44 +0200

    simplified test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstTabsPageComponent } from './pages/first-tabs-page/first-tabs-page.component';
-import { Tab1Component } from './pages/tab1/tab1.component';
-import { Tab2Component } from './pages/tab2/tab2.component';
-import { Tab3Component } from './pages/tab3/tab3.component';
-import { Tab4Component } from './pages/tab4/tab4.component';
-import { Tab5Component } from './pages/tab5/tab5.component';
-import { Tab6Component } from './pages/tab6/tab6.component';
 import { SecondTabsPageComponent } from './pages/second-tabs-page/second-tabs-page.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstTabsPageComponent, Tab1Component, Tab2Component, Tab3Component, Tab4Component, Tab5Component, Tab6Component, SecondTabsPageComponent],
+  declarations: [AppComponent, FirstTabsPageComponent, SecondTabsPageComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
